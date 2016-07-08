f4395f0da68d33b40ccfb9c26ed6cc52c2fb9495
chenxinfeng4 <529125853@qq.com>
2016-07-08 16:32:29 +0800

    增加适用于MAT文件的"LoadEngine". 2016-7-8.
    MAT文件包含
      iTime    : [n] x 1, spikes的时间。
                 单位，推荐ms。
      waveform : [n] x [chans] x [wave], spike的波形。
                 单位，推荐uV。
    启发自
      MClust3.5的 TDT2MClust.m
      MClust4.4的 LoadSEfromCSC.m
      MClust4.4的 LoadTDT.m

---
+++ MClust/LoadingEngines/LoadMAT.m
@@ -0,0 +1,59 @@
+function [t, wv] = LoadMAT(fn, R, U)
+% Loading enginer for MClust-4.4
+% inspired by 'LoadSEfromCSC.m', 'TDT2MClust.m', 'LoadTDT.m'
+% @Chen xinfeng, 2016-7-8
+
+if nargin == 1 
+    % directly load file
+    load(fn, '-mat');
+    t = iTime;
+    wv = waveform;
+elseif nargin == 2 
+    % New "get" construction
+    if strcmp(fn, 'get')
+        switch(R)
+            case 'ChannelValidity'
+                t = [true true true true];
+            case 'ExpectedExtension'
+                t = '.mat';
+            otherwise
+                error('Unknown get condition.');
+        end
+        return
+    else
+        error('2 argins requires "get" as the first argument.');
+    end
+elseif nargin == 3 
+% fn : file to load
+% R  : records_to_get, a range of values
+% U  : a flag taking one of 5 case
+    load(fn, '-mat');
+    t = iTime;
+    wv = waveform;
+    switch(U)
+        case 1 
+        % range is timestamp list
+            ind = ismember(t, R);
+            t = t(ind);
+            wv = wv(ind, :, :);
+        case 2 
+        % range is record number
+            t = t(R);
+            wv = wv(R, :, :);
+        case 3
+        % range is [start timestamp, stop timestamp]
+            ind = (t >= range(1) & t <= range(2));
+            t = t(ind);
+            wv = wv(ind, :, :);
+        case 4
+        % range is [start record, stop record]
+            ind = R(1):R(2);
+            t = t(ind);
+            wv = wv(ind, :, :);
+        case 5
+        % return the count of spikes
+            t = numel(t);
+            wv = [];
+    end
+end
+            
